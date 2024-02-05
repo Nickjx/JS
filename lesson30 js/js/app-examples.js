31ca152dc7bb86d39e645cc6e9af2b7faab88f46
Roma Boiko <102807041+Nickjx@users.noreply.github.com>
2024-02-05 22:16:42 +0200

    Add files via upload

---
+++ lesson30 js/js/app-examples.js
@@ -0,0 +1,50 @@
+// Number (IEEE 754 //Стандарт IEEE для чисел з рухомою комою (IEEE 754) — технічний стандарт формату представлення чисел з рухомою комою, створений у 1985 році Інститутом інженерів з електротехніки та електроніки (IEEE).)цілі або десяткові числа
+// 1 діапазон чисел
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MIN_SAFE_INTEGER)
+//Infinity
+//NaN не число
+// var x = 10; //ціле число
+// let y = 3.14; // десяткове число, число  з плаваючою крапкою
+
+// let num3 = 1.23e6 //1.23*(10^6)
+// let num4 = 0.00000123 //1.23e-6
+
+// let anomalia = 0.1 + 0.2 // ?
+// console.log(anomalia);
+
+// const result = 0.1 + 0.2;
+// const expected = 0.3;
+// const tolerance = 0.0001; //Припустима похибка
+// if (Math.abs(result - expected) < tolerance) {
+//   console.log('ok');
+// } else {
+//   console.log('bad');
+// }
+
+// let liam = 1000000
+// let liam1 = 1_000_000
+// let notliam2 = 1_234.567_89
+
+// let sum = 2 + 3;
+// let difference = 10 - 4;
+
+// let product = 4 * 12;
+// let quotient = 7 / 3;
+
+// let remainder1 = 10 % 3;
+// let remainder2 = 7.5 % 2.2;
+// let remainder3 = -7.5 % 2.2;
+// let remainder4 = 0 % 7;
+
+// let power = 2 ** 10; //1024
+// console.log(power);
+
+// inc++
+// let inc = 5
+// console.log(inc++);
+// inc --
+// let inc = 5
+// console.log(++inc);
+
+
